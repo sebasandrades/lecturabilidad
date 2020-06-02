8a9fc3c51ba18dff3c8bd7f17cc247d809e1c428
Sebastian <sandrades@uninorte.edu.co>
2020-06-01 19:08:22 -0500

    Correccion de error de las variables de entorno en produccion y desarrollo

--- lectura/settings.py
+++ lectura/settings.py
@@ -79,9 +79,10 @@ WSGI_APPLICATION = 'lectura.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-   'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
@@ -135,3 +136,5 @@ LOGIN_URL='/users/login'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
+if config('DJANGO_PRODUCTION_ENV', default=False, cast=bool):
+    from .settings_production import *
