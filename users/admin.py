0321f265b8e9d7b1058ce7c124e6492f9aecd42a
Sebastian <sandrades@uninorte.edu.co>
2020-06-04 21:31:03 -0500

    Modificando la vista de administrador de los resultados

--- users/admin.py
+++ users/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import Resultado
 
 
+class ResultadoAdmin(admin.ModelAdmin):
+    list_display = ('perspicuidad', 'user')
+    list_filter = ('user','perspicuidad')
 
-admin.site.register(Resultado)
+admin.site.register(Resultado,ResultadoAdmin)
